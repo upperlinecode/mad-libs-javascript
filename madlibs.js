b9be3abfc8f74b14104c88c58c27091a5de3726e
Aaron Fink <apf75@cornell.edu>
2021-03-09 00:41:20 -0500

    added index.html

--- madlibs.js
+++ madlibs.js
@@ -2,7 +2,7 @@
 let exampleNoun = prompt("Please give me a plural noun (person, place, or thing)");
 console.log("One incredible morning, Jourdan, queen of the " + exampleNoun + " woke up and made a decree.")
 console.log("'Starting tomorrow,' she said, 'all " + exampleNoun + " will need to be transformed into puppies.'")
-console.log("Jourdan went down in history as a best queen of all time.")
+console.log("Jourdan went down in history as the best queen of all time.")
 
 // Initialize your variables here.
 
